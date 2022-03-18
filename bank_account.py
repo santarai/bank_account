683d0ae04c1455affbcc47a24d382d411bb12bd8
santarai <93294060+santarai@users.noreply.github.com>
2022-03-18 15:53:10 -0400

    Add files via upload

---
+++ bank_account.py
@@ -0,0 +1,41 @@
+class BankAccount:
+    accounts = []
+    def __init__(self,int_rate,balance):
+        self.int_rate = int_rate
+        self.balance = balance
+        BankAccount.accounts.append(self)
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self,amount):
+        if(self.balance - amount) >= 0:
+            self.balance -= amount
+        else:
+            print("Insufficient Funds: Charging a $5 fee")
+            self.balance -= 5
+        return self
+    
+    def display_account_info(self):
+        print(f"Balance: {self.balance}")
+        return self
+
+    def yeild_interest(self):
+        if self.balance > 0:
+            self.balance += (self.balance * self.int_rate)
+        return self
+
+    @classmethod
+    def print_all_accounts(cls):
+        for account in cls.accounts:
+            account.display_account_info()
+
+
+savings = BankAccount(.05,1500)
+checking = BankAccount(.02,4000)
+
+savings.deposit(20).deposit(30).deposit(50).withdraw(400).yeild_interest().display_account_info()
+checking.deposit(200).deposit(300).withdraw(100).withdraw(60).withdraw(40).withdraw(80).yeild_interest().display_account_info()
+
+BankAccount.print_all_accounts()
